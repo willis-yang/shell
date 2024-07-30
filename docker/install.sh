f1a1d45373ea453dc66ab118e88ba1e825301214
willis <willis@qq.com>
2024-07-30 16:11:24 +0800

    fix(*) fix docker-compose install

--- docker/install.sh
+++ docker/install.sh
@@ -12,6 +12,16 @@ sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/dock
 # Install Docker packages
 sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+
+# install docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep -oP '"tag_name": "\K(.*)(?=")')/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+# chmod +x
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Create a symbolic link
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+
 # Start Docker service
 sudo systemctl start docker
 
